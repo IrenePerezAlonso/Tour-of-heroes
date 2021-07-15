320e8f37f7cf1428349c17e2c6a9276de07d5523
Irene Perez Alonso <irperal@hotmail.com>
2021-07-15 16:36:04 +0200

    Give a className to the add hero button

--- client/src/components/HeroList/index.jsx
+++ client/src/components/HeroList/index.jsx
@@ -9,7 +9,7 @@ function HeroList({ heroes, dispatch }) {
         <label htmlFor="new-hero">
           Hero name:
           <input id="new-hero" />
-          <button type="button">Add hero</button>
+          <button type="button" className="add-button">Add hero</button>
         </label>
       </div>
 
