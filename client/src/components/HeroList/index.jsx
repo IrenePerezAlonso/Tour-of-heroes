085da35ada7721fad017f723efdc6f768fe97216
Irene Perez Alonso <irperal@hotmail.com>
2021-07-15 17:01:01 +0200

    Import Link from react-router-dom

--- client/src/components/HeroList/index.jsx
+++ client/src/components/HeroList/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { addHero, deleteHero, loadHeroes } from '../../redux/actions/actionCreator';
