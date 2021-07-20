ff1ed77edf09182bf83305db647527fab3369c21
Irene Perez Alonso <irperal@hotmail.com>
2021-07-20 17:55:11 +0200

    Add a link to the home page into the NotFound component

--- client/src/components/NotFound/index.jsx
+++ client/src/components/NotFound/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <>
       <h1>Error 404</h1>
       <h2>Nothing to see here...</h2>
+      <Link to="/">Back to safety</Link>
     </>
   );
 }
