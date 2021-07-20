a3ff2f79d11f8d91979450a60db62472c95367af
Irene Perez Alonso <irperal@hotmail.com>
2021-07-20 17:54:12 +0200

    Add a content into the return of the NotFound component

---
+++ client/src/components/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <>
+      <h1>Error 404</h1>
+      <h2>Nothing to see here...</h2>
+    </>
+  );
+}
+
+export default NotFound;
